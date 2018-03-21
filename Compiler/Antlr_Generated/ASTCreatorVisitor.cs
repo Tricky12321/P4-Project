103257423edca0fcea41d10b8ac11c1598b76556
MStone <mstenk16@student.aau.dk>
2018-03-21 13:46:21 +0100

    FUCK YOU

---
+++ Compiler/Antlr_Generated/ASTCreatorVisitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Antlr4.Runtime.Misc;
+using Compiler.Nodes;
+
+
+namespace Compiler
+{
+    public class ASTCreator<AbstractNode> : GiraphParserBaseVisitor<AbstractNode>
+    {
+        public override AbstractNode VisitStart([NotNull] GiraphParser.StartContext context)
+		{
+            return VisitChildren(context.program(0));
+		}
+
+        public override AbstractNode VisitProgram([NotNull] GiraphParser.ProgramContext context)
+		{
+            return VisitChildren(context);
+		}
+
+        public override AbstractNode VisitFunctionDcl([NotNull] GiraphParser.FunctionDclContext context)
+		{
+            return base.VisitFunctionDcl(context);
+		}
+
+        public override AbstractNode VisitVariable([NotNull] GiraphParser.VariableContext context)
+        {
+            return base.VisitVariable(context);
+        }
+	}
+}
