53e8305db6157232dc60e0da4c9ab709b4ce4812
Tricky12321 <Thue-V-Iversen@hotmail.com>
2018-03-23 13:25:14 +0100

    Fixet GraphNodes

---
+++ Compiler/AST/Nodes/QueryNodes/SetQueryNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Compiler.AST.Nodes.QueryNodes
+{
+    public class SetQueryNode : AbstractNode
+    {
+        public Dictionary<string, string> Attributes = new Dictionary<string, string>();
+        public string VariableName;
+        public AbstractNode WhereCondition;
+        public SetQueryNode(int LineNumber) : base(LineNumber)
+        {
+        }
+    }
+}
