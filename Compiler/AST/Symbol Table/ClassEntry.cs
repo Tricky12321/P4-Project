f2fdba0c65e7722a6b978edf9a50dd53a73a8d8d
Tricky12321 <Thue-V-Iversen@hotmail.com>
2018-04-04 14:36:07 +0200

    Lavet meget om i hvordan symbol table fungere, og hvordan den checker om en variable er dcl Lavet en Extension og ClassEntry ClassEntry

---
+++ Compiler/AST/Symbol Table/ClassEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Compiler.AST.SymbolTable
+{
+    public class ClassEntry
+    {
+        public string Name;
+        public AllType Type;
+        public bool Collection = false;
+        public ClassEntry(string Name, AllType Type, bool Collection = false)
+        {
+            this.Name = Name;
+            this.Type = Type;
+            this.Collection = Collection;
+        }
+    }
+}
---
+++ Compiler/AST/Symbol Table/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Compiler.AST.Nodes;
+using Compiler.AST.Nodes.DatatypeNodes;
+using Compiler.AST.Nodes.LoopNodes;
+using Compiler.AST.Nodes.QueryNodes;
+namespace Compiler.AST.SymbolTable
+{
+    public class Extension
+    {
+        public string LongName;
+        public string ShortName;
+        public AllType Type;
+        public Extension()
+        {
+            
+        }
+    }
+}
