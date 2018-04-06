8cabefe6e1fe0b98f8e0035c6965b5f5c53e85bb
Anantonon <hezarkit@msn.com>
2018-04-06 12:42:05 +0200

    det hedder declaration!!!

--- Compiler/AST/Symbol Table/AstSymbolTableCreatorVisitor.cs
+++ Compiler/AST/Symbol Table/AstSymbolTableCreatorVisitor.cs
@@ -272,12 +272,12 @@ namespace Compiler.AST.SymbolTable
 
         public override void Visit(ForLoopNode node)
         {
-            if (node.VariableDeclartion != null) {
-                Visit(node.VariableDeclartion);
+            if (node.VariableDeclaration != null) {
+                Visit(node.VariableDeclaration);
             }
-            if (node.VariableDeclartion != null)
+            if (node.VariableDeclaration != null)
             {
-                Visit(node.VariableDeclartion);
+                Visit(node.VariableDeclaration);
             }
             VisitChildrenNewScope(node);
         }
