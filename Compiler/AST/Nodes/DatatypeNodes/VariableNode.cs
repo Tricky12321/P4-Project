134ef4a0c701eaa8e2a91806e61b6194acc284c0
MStenkaer <mstenk16@student.aau.dk>
2018-04-06 14:41:06 +0200

    Indetet

--- Compiler/AST/Nodes/DatatypeNodes/VariableNode.cs
+++ Compiler/AST/Nodes/DatatypeNodes/VariableNode.cs
@@ -5,7 +5,6 @@ namespace Compiler.AST.Nodes.DatatypeNodes
     public class VariableNode : VariableAttributeNode
     {
         public string Type;
-        public AllType Type_enum => u
         public bool Declaration = false;
         public AbstractNode Assignment;
         public VariableNode(int LineNumber, int CharIndex) : base(LineNumber, CharIndex)
