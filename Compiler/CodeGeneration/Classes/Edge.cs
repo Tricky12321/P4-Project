d65a30c76dd61ec91763bfe7db4e2972841fdd34
MStenkaer <mstenk16@student.aau.dk>
2018-04-25 13:19:29 +0200

    Edge addition

--- Compiler/CodeGeneration/Classes/Edge.cs
+++ Compiler/CodeGeneration/Classes/Edge.cs
@@ -3,12 +3,16 @@ namespace Giraph.Classes
 {
     public class Edge
     {
+        public Vertex VertexFrom;
+        public Vertex VertexTo;
+
         //EXTENSIONS HERE
         //*****EXTEND*****
         //EXTENSIONS ENDED
         public Edge(Vertex vertexFrom, Vertex vertexTo)
         {
-            
+            VertexFrom = vertexFrom;
+            VertexTo = vertexTo; 
         }
     }
 }
