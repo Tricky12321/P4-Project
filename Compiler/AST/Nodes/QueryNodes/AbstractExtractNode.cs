e343f901f03c8a258ac862f4f17b6d3f8df114af
MStenkaer <mstenk16@student.aau.dk>
2018-04-25 11:47:04 +0200

    extract er nu virksomt

---
+++ Compiler/AST/Nodes/QueryNodes/AbstractExtractNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.AST.Nodes.QueryNodes
+{
+    public class AbstractExtractNode : AbstractNode
+    {
+        public string Attribute;
+        public string Variable;
+        public AbstractNode WhereCondition;
+        public AbstractExtractNode(int LineNumber, int CharIndex) : base(LineNumber, CharIndex)
+        {
+        }
+
+        public override void Accept(AstVisitorBase astVisitor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
