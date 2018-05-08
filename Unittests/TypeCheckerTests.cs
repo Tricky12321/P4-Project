ce4e7d7e34601d5a57e0256b3cf0e3720aee5489
Matin Simonsen <Martin-ezzi@hotmail.com>
2018-05-08 14:25:24 +0200

    skal hente så mit unit testing virker

--- Unittests/TypeCheckerTests.cs
+++ Unittests/TypeCheckerTests.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using Compiler.AST.Nodes;
+using Compiler.AST.SymbolTable;
+using NUnit.Framework;
+using System;
+using Compiler;
 namespace Unittests
 {
+    [TestFixture()]
     public class TypeCheckerTest
     {
-        public TypeCheckerTest()
+        GiraphParser.StartContext CST;
+        AbstractNode AST;
+        SymTable SymbolTable;
+
+        [SetUp]
+        public void Init()
         {
-            
+            CST = Program.BuildCST("kode_TypeChecker.giraph");
+            AST = Program.BuildAST(CST);
+            SymbolTable = Program.BuildSymbolTable(AST as StartNode);
+            Program.TypeCheck(SymbolTable, AST as StartNode);
         }
+
+        [Test()]
+        public void hehetype()
+        {
+            Assert.IsTrue(true);
+        }
+
+
+
+
     }
 }
