6a7501b453cd59e0e91cc9e1acfb94464752344e
Matin Simonsen <Martin-ezzi@hotmail.com>
2018-05-25 19:04:53 +0200

    ehm, thue gjorde det her

--- Compiler/Program/Program.cs
+++ Compiler/Program/Program.cs
@@ -19,7 +19,7 @@ namespace Compiler
 	public static class Program
 	{
 		private static bool _error = false;
-		private static bool _ignoreErrors = false;
+		private static bool _ignoreErrors = true;
 		private static bool _dontDelete = true;
 		public static bool TestMode = false;
 		public static bool WriteDebugText = false;
@@ -41,10 +41,7 @@ namespace Compiler
 			ErrorChecker(_error, 9999, "Code error!");
 			AbstractNode AST = BuildAST(CST);
 			SymTable SymbolTable = BuildSymbolTable(AST as StartNode);
-			if (Utilities.GetOS() != OS.MacOS || true)
-			{
-				TypeCheck(SymbolTable, AST as StartNode);
-			}
+			TypeCheck(SymbolTable, AST as StartNode);
 			//PrettyPrint(AST as StartNode);
 			WriteCodeToFiles(AST as StartNode);
 			TotalTimer.Stop();
